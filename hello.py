4caea34c22e8aa43e4399f2688d1495d41a67153
sherry <xtaosky@live.com>
2018-04-18 22:19:06 +0800

    Signed-off-by: sherry <xtaosky@live.com>

--- hello.py
+++ hello.py
@@ -1,13 +1,42 @@
+import os
 from flask import Flask,render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+#from flask_sqlalchemy import SQLALchemy 
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'HARD TO GUESS STRING'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+	'SQLITE:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 #
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique=True)
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+
+	def __repr__(self):
+		return '<User %r>' % self.name
+		
+		
+
 
 class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators=[DataRequired()])
